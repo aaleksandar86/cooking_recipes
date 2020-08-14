7f8d872845b1d16c10ede27f848462effa1ca24b
Aleksandar Apostolov <a_aleksandar86@yahoo.com>
2020-08-14 15:20:28 +0200

    Add Recipe, Ingredient and Instruction model relation

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,2 +1,9 @@
 class Recipe < ApplicationRecord
+  belongs_to :user
+
+  has_many :ingredients, dependent: :destroy
+  has_many :instructions, dependent: :destroy
+
+  validates :title, presence: true, length: { minimum: 5 }
+  validates :description, presence: true, length: { maximum: 250 }
 end
